c6b8d3dc83407b41d806f1aadc9204114e361353
orianaluces <153571213+orianaluces@users.noreply.github.com>
2024-03-11 10:40:51 -0300

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "../Footer/Footer.css";
+
+function Footer (){
+    return(
+        <footer>
+             <p>Derechos reservados © Oriana Luces. 2024</p>
+        </footer>
+       
+    
+    )
+}
+
+export default Footer;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,44 @@
+import "../Main/Main.css";
+
+function Main() {
+    return(
+        <main>
+            <h1>Lazos más vendidos</h1>
+
+            <div>
+                <img src= "https://i.ytimg.com/vi/AnZaKAtusTw/maxresdefault.jpg" title="lazo rosa"></img>
+                <p>Valor= 10 reais</p>
+                <button>Comprar</button>
+            </div>
+            <div>
+                <img src= "https://vitrinavenezuela.com/wp-content/uploads/2022/08/20220808_153947-1-scaled.jpg" title="Par azul y blanco"></img>
+                <p>Valor= 15 reais</p>
+                <button>Comprar</button>
+            </div>
+            <div>
+                <img src= "https://falabella.scene7.com/is/image/FalabellaPE/gsc_119116787_2244025_1?wid=800&hei=800&qlt=70" title="kit escolar"></img>
+                <p>Valor= 10 reais</p>
+                <button>Comprar</button>
+            </div>
+            <div>
+                <img src= "https://i.pinimg.com/564x/d8/cc/54/d8cc54e575e0d76a8d2708ef0c3c3521.jpg" title="Lazo Minnie"></img>
+                <p>Valor= 10 reais</p>
+                <button>Comprar</button>
+            </div>
+            <div>
+                <img src= "https://i.pinimg.com/564x/d1/57/c0/d157c0d530f706608115dec6e6a67be6.jpg" title="Lazo coquette"></img>
+                <p>Valor= 10 reais</p>
+                <button>Comprar</button>
+            </div>
+            <div>
+                <img src= "https://i.pinimg.com/236x/49/9f/0b/499f0b33f412c439c9ea08ddf8ecfb0c.jpg" title="Lazo Rosa"></img>
+                <p>Valor= 10 reais</p>
+                <button>Comprar</button>
+            </div>
+
+        </main>
+    )
+
+}
+
+export default Main;
